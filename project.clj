86858f06fa21c9d765b2e9ed3d6a8d11ea6c3f7f
Alexander Kiel <alexanderkiel@gmx.net>
2015-08-10 18:04:22 +0200

    Release v0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.akiel/ring-hap "0.2-SNAPSHOT"
+(defproject org.clojars.akiel/ring-hap "0.2"
   :description "Ring Middleware for Hypermedia Application Protocol."
   :url "https://github.com/alexanderkiel/ring-hap"
 
