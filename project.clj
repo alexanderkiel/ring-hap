d1935f132a2ce70d79d52aec0bc0918b5ba52d68
Alexander Kiel <alexanderkiel@gmx.net>
2016-01-20 15:51:18 +0100

    Update Clojure to 1.8.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring/ring-core "1.3.2" :exclusions [clj-time
                                                       commons-codec
                                                       commons-fileupload
